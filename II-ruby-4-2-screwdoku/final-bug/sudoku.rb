e810362085b84d8ceb4ae1c49c30d7ff3987d4b8
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-10-12 12:15:57 +0200

    II-ruby-4-2-screwdoku final-bug

--- II-ruby-4-2-screwdoku/final-bug/sudoku.rb
+++ II-ruby-4-2-screwdoku/final-bug/sudoku.rb
@@ -1,4 +1,8 @@
 require_relative "board"
+require "colorize"
+require "byebug"
+
+# puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
@@ -10,15 +14,42 @@ class SudokuGame
     @board = board
   end
 
+  def method_missing(method_name, *args)
+    if method_name =~ /val/
+      Integer(1)
+    else
+      string = args[0]
+      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position" }
+    end
+  end
+
+  def parse_pos(pos)
+    pos.split(",").map { |char| Integer(char) }
+  end
+
   def get_pos
     pos = nil
+    # debugger
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
-      # begin
-      pos = parse_pos(gets.chomp)
-      # rescue
+
+      begin
+        pos = parse_pos(gets.chomp)
+      rescue
+        # debugger
+        $!.backtrace # => array of backtrace steps
+        # error.backtrace
+        # rescue => error
+        # error and $! are equivalent here
+        # TODO: Google how to print the error that happened inside of a rescue statement.
+        puts "Invalid position entered (did you use a comma?)"
+        puts ""
+
+        pos = nil
+      end
     end
+
     pos
   end
 
@@ -32,14 +63,6 @@ class SudokuGame
     val
   end
 
-  def parse_pos(string)
-    string.split(",").map { |char| Integer(char) }
-  end
-
-  def parse_val(string)
-    Integer(string)
-  end
-
   def play_turn
     board.render
     pos = get_pos
@@ -48,6 +71,7 @@ class SudokuGame
   end
 
   def run
+    # debugger
     play_turn until solved?
     board.render
     puts "Congratulations, you win!"
@@ -72,3 +96,6 @@ class SudokuGame
 
   attr_reader :board
 end
+
+game = SudokuGame.from_file("puzzles/sudoku1-solved.txt")
+game.run
