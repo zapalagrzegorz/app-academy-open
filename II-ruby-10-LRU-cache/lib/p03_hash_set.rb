045c1501dd9dec6d6a3684e554621b047ba9ba3f
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-01-25 12:40:54 +0100

    Completes simple HashSet of LRU cache

--- II-ruby-10-LRU-cache/lib/p03_hash_set.rb
+++ II-ruby-10-LRU-cache/lib/p03_hash_set.rb
@@ -1,24 +1,37 @@
+# frozen_string_literal: true
+
 class HashSet
   attr_reader :count
 
   def initialize(num_buckets = 8)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
     @count = 0
   end
 
   def insert(key)
+    resize! if @count + 1 == num_buckets
+    unless include?(key)
+      self[key] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    return unless include?(key)
+
+    self[key].delete(key)
+    @count -= 1
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,11 @@ class HashSet
   end
 
   def resize!
+    # map existing items into new array twice big
+    resized_store = Array.new(num_buckets * 2) { [] }
+    @store.each do |sub_arr|
+      sub_arr.each { |element| resized_store[element.hash % resized_store.size] << element }
+    end
+    @store = resized_store
   end
 end
