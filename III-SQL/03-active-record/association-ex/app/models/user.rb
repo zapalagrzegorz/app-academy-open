006c21a146bd59b218a37f81051bf8fd6ee72918
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-03-30 23:41:32 +0200

    Association ex - completed User and Enrollements

---
+++ III-SQL/03-active-record/association-ex/app/models/user.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+  # enrollments and enrolled_courses.
+  has_many :enrollments,
+           primary_key: :id,
+           foreign_key: :student_id,
+           class_name: 'Enrollment'
+
+  has_many :enrolled_courses, through: :enrollments, source: :course
+  # prerequisite
+  #  prereq_id
+end
