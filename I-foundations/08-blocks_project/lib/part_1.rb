52066cca0a223b088f4a034567fa8037f89025a8
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-04-05 15:03:48 +0200

    Saving solution to Procs chapter

--- I-foundations/08-blocks_project/lib/part_1.rb
+++ I-foundations/08-blocks_project/lib/part_1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def select_even_nums(numArr)
   numArr.select(&:even?)
 end
@@ -6,19 +8,21 @@ def reject_puppies(arrHash)
   arrHash.reject { |dog| dog['age'] <= 2 }
 end
 
+# #count
+# If a block is given, it counts the number of elements yielding a true value.
 def count_positive_subarrays(arr2d)
   arr2d.count { |el| el.sum > 0 }
 end
 
 def aba_translate(string)
   encoded = ''
-  
-  string.each_char do |char| 
-    if 'aeuoi'.include?(char)
-      encoded += char + "b" + char
-    else
-      encoded += char
-    end
+
+  string.each_char do |char|
+    encoded += if 'aeuoi'.include?(char)
+                 char + 'b' + char
+               else
+                 char
+               end
   end
 
   encoded
@@ -27,4 +31,3 @@ end
 def aba_array(arrWords)
   arrWords.map { |word| aba_translate(word) }
 end
-
