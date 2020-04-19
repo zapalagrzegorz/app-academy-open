b8a014edb84993e1c5439d901ae6d70c09e8c56d
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-04-19 12:59:33 +0200

    Complete n+1 buster exercise

---
+++ III-SQL/04-active-record-II/n+1_buster/app/models/house.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :bigint           not null, primary key
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class House < ApplicationRecord
+  has_many :gardeners,
+           class_name: 'Gardener',
+           foreign_key: :house_id,
+           primary_key: :id
+
+  has_many :plants,
+           through: :gardeners,
+           source: :plants
+
+  def n_plus_one_seeds
+    plants = self.plants
+    seeds = []
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+
+    seeds
+  end
+
+  def better_seeds_query
+    # articles: [{ comments: :guest }
+    plants = self.plants.includes(:seeds)
+    seeds = []
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+    # TODO: your code here
+    seeds
+  end
+end
