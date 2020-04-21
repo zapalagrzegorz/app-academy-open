2f4acb4fa55a99d1d03db0371fff2876b095dbba
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-04-21 22:36:26 +0200

    Completes 1/3 part of Active Record query exercise

--- III-SQL/04-active-record-II/n+1_buster/app/models/route.rb
+++ III-SQL/04-active-record-II/n+1_buster/app/models/route.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: routes
@@ -9,9 +11,9 @@
 #
 class Route < ApplicationRecord
   has_many :buses,
-    class_name: 'Bus',
-    foreign_key: :route_id,
-    primary_key: :id
+           class_name: 'Bus',
+           foreign_key: :route_id,
+           primary_key: :id
 
   def n_plus_one_drivers
     buses = self.buses
@@ -30,5 +32,18 @@ class Route < ApplicationRecord
 
   def better_drivers_query
     # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
