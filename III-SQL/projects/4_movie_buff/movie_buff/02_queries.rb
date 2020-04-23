9e6f2ac6ed7cac134a5ebeef616b6b17f0030753
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-04-23 09:03:38 +0200

    Add 2 queries to movie_buff, part 2

--- III-SQL/projects/4_movie_buff/movie_buff/02_queries.rb
+++ III-SQL/projects/4_movie_buff/movie_buff/02_queries.rb
@@ -1,19 +1,46 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie.select(:id, :title, :yr, :score).where(score: 3..5, yr: 1980..1989)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
+  # good_years = Movie.select(:yr).distinct.where('score > 8 ').pluck(:yr)
+  Movie.select(:yr).distinct.where('yr NOT IN (SELECT DISTINCT movies.yr FROM movies WHERE score > 8)').pluck(:yr)
 
+  # Movie.group(:yr).having('MAX(score) < 8').pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
 end
 
 def vanity_projects
@@ -22,11 +49,9 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
 end
