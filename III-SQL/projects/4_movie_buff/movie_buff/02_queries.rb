e43051cdf2a3769fabf02610cb524e05af547a35
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-04-25 15:49:53 +0200

    Complete queries for 'Movie_buff'

--- III-SQL/projects/4_movie_buff/movie_buff/02_queries.rb
+++ III-SQL/projects/4_movie_buff/movie_buff/02_queries.rb
@@ -35,12 +35,14 @@ def bad_years
   # good_years = Movie.select(:yr).distinct.where('score > 8 ').pluck(:yr)
   Movie.select(:yr).distinct.where('yr NOT IN (SELECT DISTINCT movies.yr FROM movies WHERE score > 8)').pluck(:yr)
 
+  # solution:
   # Movie.group(:yr).having('MAX(score) < 8').pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  Actor.select(:id, :name).joins(:movies).where(movies: { title: title }).order(ord: :asc)
 end
 
 def vanity_projects
@@ -49,9 +51,26 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movie.select('movies.id, movies.title, actors.name').joins(:actors).where('movies.director_id = actors.id AND castings.ord = 1')
+  # .where('movies.id = actors.director_id')
+  # 1 nie wszystkie pola z zadania
+  # 2 błedna nazwa pola w tabeli
+  # 3 błędne odczytanie referencji belongs to - foreign_id jest w tabeli, która jest belongs_tos
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+
+  Actor.select(:id, :name, 'COUNT(actors.id) AS roles').joins(:castings).where.not(castings: { ord: 1 }).group('actors.id').order('COUNT(actors.id) DESC').limit(2)
+  #  .where.not(castings: { ord: 1 })
+  # .select(:id, :name, 'COUNT(castings.actor_id) as roles')
+
+  # Actor
+  #   .select(:id, :name, 'COUNT(castings.actor_id) as roles')
+  #   .joins(:castings)
+  #   .where.not(castings: { ord: 1 })
+  #   .group('actors.id')
+  #   .order('roles DESC')
+  #   .limit(2)
 end
--- III-SQL/projects/4_movie_buff/movie_buff/03_queries.rb
+++ III-SQL/projects/4_movie_buff/movie_buff/03_queries.rb
@@ -1,24 +1,64 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  Movie.select(:id, :title).joins(:actors).where(actors: { name: those_actors }).group(:id).having('COUNT(actors.id) >= ?', those_actors.length)
+  # solution nie jest na to odpowiedzią, bo działa tylko jako logicznie OR przez użycie WHERE actors.name IN ...
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+  # 1917 / 10 - 191
+  # 1920 / 10 - 192 * 10 = 1920
+  # Korzystając z możliwości grupowania, musimy być świadomi pewnej logicznej konsekwencji. We wszystkich kolejnych krokach przetwarzania zapytania następujących po klauzuli GROUP BY
+  #  (czyli w HAVING, SELECT, ORDER BY), będziemy mogli bezpośrednio wybierać tylko dane z kolumn sekcji grupującej. Wszystkie pozostałe kolumny, zawarte w sekcji surowej, mogą być wyciągane i przetwarzane tylko za pośrednictwem funkcji agregujących.
 
+  # .first zwraca obiekt .decade zwraca props
+  Movie.select('AVG(score), (yr / 10) * 10 AS decade').group('decade').order('AVG(score) DESC').limit(1).first.decade
+  # group
+  # pluck
+  # AVG(:score) AS avg_score
+  # order(avg_score DESC)
+  # limit(1)
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
+  subquery = Movie.select(:id).joins(:actors).where(actors: { name: name })
 
+  Actor.select(:name).distinct.joins(:movies).where.not(name: name).where(movies: { id: subquery }).pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor.left_outer_joins(:movies).where(movies: { id: nil }).count
 end
 
 def starring(whazzername)
@@ -26,9 +66,24 @@ def starring(whazzername)
   # A name is like whazzername if the actor's name contains all of the
   # letters in whazzername, ignoring case, in order.
 
+  # lester stone
+
+  # Sylvester Stallone
+  # lester stone
+
+  # Syv all
+  # one
+  # st
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
+  Movie.joins(:actors).where('actors.name LIKE ?', '%' + whazzername + '%')
+  # Movie.joins(:actors).where("actors.name LIKE '%Stallone%'")
 
+  # zamień 'word' na "w%o%r%d"
+  matcher = "%#{whazzername.split(//).join('%')}%"
+
+  # metoda UPPER dla kolumny oraz szukanej wartości pozwala na case-ignore
+  Movie.joins(:actors).where('UPPER(actors.name) LIKE UPPER(?)', matcher)
 end
 
 def longest_career
@@ -36,5 +91,15 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
+  Actor.select(:id, :name, 'MAX(movies.yr) - MIN(movies.yr) AS career').joins(:movies).order('career DESC, name').group(:id).limit(3)
+      #  .order('career DESC, name')
+  # group(  )
+  # .select(:name, :id, 'MAX(movies.yr) - MIN(movies.yr) AS career')
 
+    # Actor
+    # .select(:name, :id, 'MAX(movies.yr) - MIN(movies.yr) AS career')
+    # .joins(:movies)
+    # .order('career DESC, name')
+    # .group(:id)
+    # .limit(3)
 end
