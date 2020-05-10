8357931c9b75027a5083b05f55d4637470ba35f9
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-05-10 11:56:07 +0200

    Sumplement SQLObject with "where" method

--- III-SQL/05-active-record-under-the-hood/AR-under-the-hood/lib/02_searchable.rb
+++ III-SQL/05-active-record-under-the-hood/AR-under-the-hood/lib/02_searchable.rb
@@ -1,12 +1,26 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    values = []
+    where_line = []
+    params.each do |k, v| 
+      where_line << "#{k} = ?" 
+      values << v
+    end
+
+    sql_result = DBConnection.execute(<<-SQL, values)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{where_line.join(' AND ')}
+    SQL
+
+    parse_all(sql_result)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
