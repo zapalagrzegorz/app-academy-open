e7617a592434e58ef15b1ff5f6c007fbf2db29ea
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-05-11 22:55:42 +0200

    Saves progress on 'Associatable' module

--- III-SQL/05-active-record-under-the-hood/AR-under-the-hood/lib/03_associatable.rb
+++ III-SQL/05-active-record-under-the-hood/AR-under-the-hood/lib/03_associatable.rb
@@ -1,53 +1,84 @@
+# frozen_string_literal: true
+
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
-    :class_name,
-    :primary_key
+    :primary_key,
+    :class_name
   )
 
   def model_class
-    # ...
-    # After you have these basic defaults working, write #model_class, which should use String#constantize to go from a class name to the class object.
     class_name.constantize
   end
 
   def table_name
-    # ...
     class_name.downcase.underscore + 's'
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @foreign_key = options[:foreign_key] || (name.to_s.underscore + "_id").to_sym
+    @foreign_key = options[:foreign_key] || (name.to_s.underscore + '_id').to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.camelcase
-
-
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] || (self_class_name.singularize.underscore + "_id").to_sym
-    @class_name = options[:class_name] || name.singularize.camelcase
+    @foreign_key = options[:foreign_key] || (self_class_name.to_s.singularize.underscore + '_id').to_sym
 
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
-  # Phase IIIb
+  #   Phase IIIb: belongs_to, has_many
+
+  # Begin writing a belongs_to method for Associatable. This method should take in the association name and an options hash. It should build a BelongsToOptions object; save this in a local variable named options.
+
+  # Within belongs_to, call define_method to create a new method to access the association. Within this method:
+
+  #     Use send to get the value of the foreign key.
+  #     Use model_class to get the target model class.
+  #     Use where to select those models where the primary_key column is equal to the foreign key value.
+  #     Call first (since there should be only one such item).
+
+  # Throughout this method definition, use the options object so that defaults are used appropriately.
   def belongs_to(name, options = {})
-    # ...
+    # define_method to create a new method to access the association
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      # options.foreign_key - nazwa kolumny
+      # send(foreign_key) - wartość
+
+      foreign_key_value = send(options.foreign_key)
+
+      where_line = { "#{options.primary_key}": foreign_key_value }
+
+      options.model_class.where(where_line).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self, options)
+
+    define_method(name) do
+      primary_key_value = send(options.primary_key)
+
+      where_line = { "#{options.foreign_key}": primary_key_value }
+
+      options.model_class.where(where_line)
+    end
+
+    # debugger
   end
 
   def assoc_options
@@ -57,4 +88,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
