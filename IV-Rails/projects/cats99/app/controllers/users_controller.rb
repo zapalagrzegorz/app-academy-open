40d28a6b873c268523ed0f11ee086e0e976e7d17
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-06-25 06:45:52 +0200

    Add first mailer and helper

--- IV-Rails/projects/cats99/app/controllers/users_controller.rb
+++ IV-Rails/projects/cats99/app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver_now
+
       login!(@user)
       flash[:success] = 'User successfully created.'
       redirect_to root_url
