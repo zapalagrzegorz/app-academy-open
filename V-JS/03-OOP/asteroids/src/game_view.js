75920c1e403f288e7906f9e1fd15f06ef8b4a632
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-09-04 17:02:00 +0200

    Moving the ship

--- V-JS/03-OOP/asteroids/src/game_view.js
+++ V-JS/03-OOP/asteroids/src/game_view.js
@@ -1,16 +1,20 @@
+/* globals key */
+
 // Your GameView class will be responsible for keeping track of the canvas context, the game, and the ship. Your GameView will be in charge of setting an interval to animate your game. In addition, it will eventually bind key handlers to the ship so that we can move it around.
 
 // Define an GameView class in src/game_view.js. The GameView should store a Game and take in and store a drawing ctx.
 
 // Write a GameView.prototype.start method. It should call setInterval to call Game.prototype.moveObjects and Game.prototype.draw once every
 
-function GameView(game, ctx){
+import key from 'keymaster';
+
+function GameView(game, ctx) {
   this.game = game;
   this.ctx = ctx;
 }
 
-GameView.prototype.start = function() {
- 
+GameView.prototype.start = function () {
+  this.bindKeyHandlers();
 
   const step = () => {
     this.game.draw(this.ctx);
@@ -19,6 +23,26 @@ GameView.prototype.start = function() {
   };
 
   window.requestAnimationFrame(step);
+};
+
+GameView.prototype.bindKeyHandlers = function () {
+  // space, up, down, left, right
+  key('up', () => {
+    this.game.ship.power([0, -1]);
+  });
+  key('down', () => {
+    this.game.ship.power([0, 1]);
+  });
+  key('left', () => {
+    this.game.ship.power([-1, 0]);
+  });
+  key('right', () => {
+    this.game.ship.power([1, 0]);
+  });
+
+  key('space', () => {
+    // this.game.ship.power([1, 0]);
+  });
 };
 
 export default GameView;
--- V-JS/03-OOP/asteroids/src/ship.js
+++ V-JS/03-OOP/asteroids/src/ship.js
@@ -1,20 +1,30 @@
 import Util from './utils';
 import MovingObject from './moving_object';
 
-function Ship(options){
+function Ship(options) {
   options.color = Ship.COLOR;
   options.radius = Ship.RADIUS;
-  options.vel = [0,0];
+  options.vel = [0, 0];
   MovingObject.call(this, options);
 }
 
 Util.inherits(Ship, MovingObject);
-Ship.COLOR = '#fe019a' ;
+Ship.COLOR = '#fe019a';
 Ship.RADIUS = '13';
+Ship.MAX_SPEED = 7;
 
-Ship.prototype.relocate = function(){
-  this.vel = [0,0];
+Ship.prototype.relocate = function () {
+  this.vel = [0, 0];
   this.pos = this.game.randomPosition();
+};
+
+Ship.prototype.power = function (impulse) {
+  this.vel[0] += impulse[0];
+  this.vel[1] += impulse[1];
+  if (this.vel[0] > Ship.MAX_SPEED) this.vel[0] = Ship.MAX_SPEED;
+  if (this.vel[0] < -Ship.MAX_SPEED) this.vel[0] = -Ship.MAX_SPEED;
+  if (this.vel[1] > Ship.MAX_SPEED) this.vel[0] = Ship.MAX_SPEED;
+  if (this.vel[1] < -Ship.MAX_SPEED) this.vel[0] = -Ship.MAX_SPEED;
 };
 
 export default Ship;
