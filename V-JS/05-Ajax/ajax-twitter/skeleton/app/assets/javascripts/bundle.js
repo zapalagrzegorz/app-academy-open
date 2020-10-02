b60832a6eaa553ccec3440135bc64633647259fb
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-10-02 10:45:21 +0200

    Adding and removing user mention select input

--- V-JS/05-Ajax/ajax-twitter/skeleton/app/assets/javascripts/bundle.js
+++ V-JS/05-Ajax/ajax-twitter/skeleton/app/assets/javascripts/bundle.js
@@ -229,19 +229,27 @@ const MAX_TWEET_CHARS = 140;
 class TweetCompose {
   constructor(el) {
     this.$el = el;
-    this.$inputs = this.$el.find(':input');
     this.$textArea = this.$el.find('textarea');
+    this.users = this.setUsers();
+    this.mentionedUsersContainer = this.$el.find('.mentioned-users');
     this.$el.on('submit', (e) => {
       this.submit(e);
     });
+    this.$el.on('click', '.remove-mentioned-user', (e) => {
+      this.removeMention(e);
+    });
     this.$textArea.on('input', () => {
       this.countCharsLeft();
+    });
+    this.$el.find('#addMention').on('click', () => {
+      this.addMention();
     });
     this.countCharsLeft();
   }
 
   submit(e) {
     e.preventDefault();
+    const $inputs = this.$el.find(':input');
     const formData = this.$el.serialize();
     this.$el.find(':input').prop('disabled', true);
     API_UTIL.createTweet(formData)
@@ -257,17 +265,16 @@ class TweetCompose {
         console.error(qXHR, textStatus, errorThrown);
       })
       .always(() => {
-        const inputs = this.$inputs;
-        inputs.each((_, el) => {
+        $inputs.each((_, el) => {
           $(el).prop('disabled', false);
         });
       });
   }
 
   clearInput() {
-    this.$inputs.not('[type="submit"], [type="hidden"]').each((_, el) => {
-      $(el).val('');
-    });
+    this.$textArea.val('');
+
+    this.countCharsLeft();
   }
 
   handleSuccess() {
@@ -287,6 +294,7 @@ class TweetCompose {
         </li>`;
       });
     }
+
     if (mentionedUsers) {
       mentions = `<ul>${mentionedUsers}</ul>`;
     }
@@ -305,6 +313,34 @@ class TweetCompose {
   countCharsLeft() {
     const charsLeft = MAX_TWEET_CHARS - this.$textArea.val().length;
     $('.chars-left').text(charsLeft);
+  }
+
+  setUsers() {
+    const rawUsers = this.$el.find('.mentioned-users');
+    const usersInvalidJSON = rawUsers.get(0).dataset.users.split('');
+    usersInvalidJSON.splice(usersInvalidJSON.lastIndexOf(','), 1);
+    const validUsers = usersInvalidJSON.join('');
+    return JSON.parse(validUsers);
+  }
+
+  addMention() {
+    const usersTemplate = this.users.map(
+      (user) => ` <option value="${user.id}">${user.username}</option>`
+    );
+
+    const template = `
+    <li>
+      <button class="remove-mentioned-user">X</button>
+      <select name="tweet[mentioned_user_ids][]">
+        ${usersTemplate}
+      </select>
+    </li>`;
+
+    this.mentionedUsersContainer.append(template);
+  }
+
+  removeMention(e) {
+    e.currentTarget.parentElement.remove();
   }
 }
 
