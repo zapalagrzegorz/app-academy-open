4483456f7720709c4efe61bd6d321771fabb3c02
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-11-07 07:42:15 +0100

    Aktualizacje uboczne z projektu Ajax Twitter

--- V-JS/02-event-loop/myDebounce.js
+++ V-JS/02-event-loop/myDebounce.js
@@ -1,7 +1,7 @@
 // myDebounce
-// określa czas bezwywołania podstawowej funkcji
+// określa minimalny czas do wywołania funkcji
 // jeżeli funkcja będzie wywołana w podanym okresie ponownie, anuluje dotychczasowe
-// wywołanie i zakolejkuje ostatnie do wywołania w danym oknie czasowym
+// oczekiwanie na wywołanie i zakolejkuje nowe
 Function.prototype.myDebounce = function (interval) {
   let timeout;
   return (...args) => {
@@ -9,7 +9,7 @@ Function.prototype.myDebounce = function (interval) {
     const fnCall = () => {
       timeout = null;
       this(...args);
-    }
+    };
 
     clearTimeout(timeout);
     timeout = setTimeout(fnCall, interval);
@@ -18,7 +18,7 @@ Function.prototype.myDebounce = function (interval) {
 
 class SearchBar {
   constructor() {
-    this.query = "";
+    this.query = '';
 
     this.type = this.type.bind(this);
     this.search = this.search.bind(this);
@@ -39,17 +39,17 @@ const searchBar = new SearchBar();
 searchBar.search = searchBar.search.myDebounce(500);
 
 const queryForHelloWorld = () => {
-  searchBar.type("h");
-  searchBar.type("e");
-  searchBar.type("l");
-  searchBar.type("l");
-  searchBar.type("o");
-  searchBar.type(" ");
-  searchBar.type("w");
-  searchBar.type("o");
-  searchBar.type("r");
-  searchBar.type("l");
-  searchBar.type("d");
+  searchBar.type('h');
+  searchBar.type('e');
+  searchBar.type('l');
+  searchBar.type('l');
+  searchBar.type('o');
+  searchBar.type(' ');
+  searchBar.type('w');
+  searchBar.type('o');
+  searchBar.type('r');
+  searchBar.type('l');
+  searchBar.type('d');
 };
 
 queryForHelloWorld();
