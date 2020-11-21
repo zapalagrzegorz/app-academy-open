347450079c9b22b2e2da2883bfd9b602b6412d34
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-11-21 15:34:57 +0100

    openMail - reading message store

---
+++ V-JS/06-JS-DOM/openMail/src/message-store.js
@@ -0,0 +1,52 @@
+const messages = {
+  sent: [
+    {
+      to: 'friend@gmail.com',
+      subject: 'check my message',
+      body: "it's good enough",
+    },
+    { to: 'person@mail.com', subject: 'zzz', body: 'so booring' },
+  ],
+  inbox: [
+    {
+      from: 'grandma@mail.com',
+      subject: 'Fwd: Fwd: Fwd: Check this out',
+      body: 'Stay at home mom discovers cure for leg cramps. Doctors hate her',
+    },
+    {
+      from: 'person@mail.com',
+      subject: 'Questionnaire',
+      body: 'Take this free quiz win $1000 dollars',
+    },
+  ],
+};
+
+export const MessageStore = {
+  // a function that returns the array at messages.inbox
+  getInboxMessages() {
+    return messages.inbox;
+  },
+  // : a function that returns the array at messages.sent.
+  getSentMessages() {
+    return messages.sent;
+  },
+
+  updateDraftField(field, value) {
+    this.messageDraft[field] = value;
+  },
+  sendDraft() {
+    messages.sent.push(this.messageDraft);
+    this.messageDraft = new Message();
+  },
+
+  messageDraft: new Message(),
+};
+
+class Message {
+  constructor(from, to, subject, body) {
+    this.from = from;
+    this.to = to;
+    this.subject = subject;
+    this.body = body;
+  }
+}
--- V-JS/06-JS-DOM/openMail/src/sent.js
+++ V-JS/06-JS-DOM/openMail/src/sent.js
@@ -1,8 +1,22 @@
+import { MessageStore } from './message-store';
+
 export const Sent = {
   render() {
     const list = document.createElement('ul');
     list.classList.add('messages');
-    list.innerHTML = 'sent messages';
+
+    const messages = MessageStore.getSentMessages().map(this.renderMessage);
+    list.append(...messages);
     return list;
+  },
+  renderMessage(message) {
+    const { to, subject, body } = message;
+    const li = document.createElement('li');
+    li.classList.add('message');
+    li.innerHTML = `<span class="to">To: ${to}</span>
+    <span class="subject">subject: ${subject}</span>
+    <span class="body">${body}</span>
+    `;
+    return li;
   },
 };
