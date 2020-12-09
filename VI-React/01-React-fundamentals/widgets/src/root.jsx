ed9b83d738527c94da7c9adb9769edf7f66c0146
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-12-09 12:12:29 +0100

    React foundations - wigdets - clock

---
+++ VI-React/01-React-fundamentals/widgets/src/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Clock } from './clock';
+
+export function Root() {
+  return (
+    <div>
+      <Clock />;
+    </div>
+  );
+}
---
+++ VI-React/01-React-fundamentals/widgets/src/scheme.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstValue: '',
+      secondValue: '',
+      result: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOperation = this.handleOperation.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+  }
+
+  handleChange(e) {
+    const value = e.target.value;
+    if (value != '') {
+      this.setState({
+        [e.target.name]: parseFloat(value),
+      });
+    } else {
+      this.setState({
+        [e.target.name]: value,
+      });
+    }
+  }
+
+  handleClear(e) {
+    e.preventDefault();
+    this.setState({
+      firstValue: '',
+      secondValue: '',
+    });
+  }
+
+  handleOperation(e) {
+    // alert('A name was submitted: ' + value);
+    e.preventDefault();
+    const operation = e.target.dataset.type;
+    const { firstValue, secondValue } = this.state;
+    switch (operation) {
+      case 'addition':
+        this.setState({
+          result: firstValue + secondValue,
+        });
+        break;
+      case 'substract':
+        this.setState({
+          result: firstValue - secondValue,
+        });
+        break;
+      case 'multiply':
+        this.setState({
+          result: firstValue * secondValue,
+        });
+        break;
+      case 'divide':
+        this.setState({
+          result: firstValue / secondValue,
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    const { result, firstValue, secondValue } = this.state;
+    return (
+      <div>
+        <div className="result">{result}</div>
+        <div className="inputs">
+          <input
+            type="number"
+            id="first-value"
+            name="firstValue"
+            value={firstValue}
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            id="second-value"
+            name="secondValue"
+            value={secondValue}
+            onChange={this.handleChange}
+          />
+
+          <input type="reset" value="Clear" onClick={this.handleClear} />
+        </div>
+        <div className="btns">
+          <OperationBtn
+            btnType="addition"
+            handleOperation={this.handleOperation}
+          >
+            +
+          </OperationBtn>
+          <OperationBtn
+            btnType="substract"
+            handleOperation={this.handleOperation}
+          >
+            -
+          </OperationBtn>
+          <OperationBtn
+            btnType="multiply"
+            handleOperation={this.handleOperation}
+          >
+            *
+          </OperationBtn>
+          <OperationBtn btnType="divide" handleOperation={this.handleOperation}>
+            /
+          </OperationBtn>
+        </div>
+      </div>
+    );
+  }
+}
