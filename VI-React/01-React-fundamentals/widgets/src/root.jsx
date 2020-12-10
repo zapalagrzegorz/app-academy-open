201980352d78edc60db4f80499d9b6eb1ef3af01
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2020-12-10 13:06:50 +0100

    React foundations - tabs widget

--- VI-React/01-React-fundamentals/widgets/src/root.jsx
+++ VI-React/01-React-fundamentals/widgets/src/root.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Clock } from './clock';
+import { Tabs } from './tabs';
 
 export function Root() {
+  const tabs = [
+    { title: 'tab1', content: 'content1' },
+    { title: 'tab2', content: 'content2' },
+    { title: 'tab3', content: 'content3' },
+  ];
   return (
-    <div>
-      <Clock />;
-    </div>
+    <>
+      <Clock />
+      <Tabs tabs={tabs} />
+    </>
   );
 }
