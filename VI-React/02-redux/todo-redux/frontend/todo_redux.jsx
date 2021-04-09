44f7a816dee7fe43ad9f3d30742323b810ab123c
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2021-04-09 18:06:15 +0200

    Basics of redux middlewares

--- VI-React/02-redux/todo-redux/frontend/todo_redux.jsx
+++ VI-React/02-redux/todo-redux/frontend/todo_redux.jsx
@@ -27,20 +27,45 @@ const newSteps = [
 
 const newStep = { id: 3, title: 'buy lolipop', done: false, todo_id: 4 };
 
-window.store = configureStore();
+const applyMiddlewares = (store, ...middlewares) => {
+  // Create a variable dispatch, setting it equal to store.dispatch
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    // Reassign dispatch to the result of middleware(store)(dispatch)
+    dispatch = middleware(store)(dispatch);
+    // What is next inside the logging function? It's store.dispatch
+  });
+  return Object.assign({}, store, { dispatch });
+};
 
-window.store.getState();
-window.store.dispatch(receiveTodos(newState));
-window.store.getState();
-window.store.dispatch(deleteTodo(4));
-window.store.getState();
-window.store.dispatch(steps.receiveSteps(newSteps));
-window.store.getState();
-window.store.dispatch(steps.receiveStep(newStep));
-window.store.getState();
-window.store.dispatch(steps.deleteStep(3));
-window.store.getState();
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  // Create a variable dispatch, setting it equal to store.dispatch
+  console.log('logging via original middleware');
+  console.log(store.getState());
+  console.log(action);
+  // dispatch(action)
+  next(action);
+  console.log(store.getState());
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
+
+  window.store = configureStore();
+  // console.log(window.store.getState());
+
+  // const newStore = applyMiddlewares(window.store, addLoggingToDispatch);
+
+  // window.store = newStore;
+  // window.store.getState();
+  window.store.dispatch(receiveTodos(newState));
+  // window.store.getState();
+  window.store.dispatch(deleteTodo(4));
+  // window.store.getState();
+  window.store.dispatch(steps.receiveSteps(newSteps));
+  // window.store.getState();
+  window.store.dispatch(steps.receiveStep(newStep));
+  // window.store.getState();
+  window.store.dispatch(steps.deleteStep(3));
+  // window.store.getState();
 });
