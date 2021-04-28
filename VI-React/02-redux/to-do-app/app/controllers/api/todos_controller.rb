a46ab0faec8e07664271190ec0fffcacb69c9997
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2021-04-28 15:55:01 +0200

    Todos controller

--- VI-React/02-redux/to-do-app/app/controllers/api/todos_controller.rb
+++ VI-React/02-redux/to-do-app/app/controllers/api/todos_controller.rb
@@ -1,16 +1,49 @@
 class Api::TodosController < ApplicationController
-  def show
+  def index
+    # query = request.query_parameters[:username]
+    render json: Todo.all
   end
 
-  def index
+  def show
+    todo = Todo.find_by(id: params[:id])
+
+    if todo
+      render json: todo
+    else
+      render plain: 'Not found', status: :not_found
+    end
   end
 
   def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: todo.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def update
+     todo = Todo.find_by(id: params[:id])
+    # todo.update
+
+    if todo
+      todo.update(user_params)
+      render json: todo
+    else
+      render json: todo.errors.full_messages, status: :unprocessable_entity
+    end
+  
   end
 
   def destroy
+     todo = Todo.find_by(id: params[:id])
+
+    if todo
+      todo.destroy
+      render json: todo
+    else
+      render plain: '', status: :not_found
+    end
   end
 end
